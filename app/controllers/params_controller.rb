e9efa2413101b5aab013561146f693213898855d
Mitchel Summers <mitchel.a.summers@gmail.com>
2021-10-14 11:40:56 -0600

    adds working name and guess querys

--- app/controllers/params_controller.rb
+++ app/controllers/params_controller.rb
@@ -1,14 +1,27 @@
 class ParamsController < ApplicationController
   def name_enter
     input_value = params["your_name"]
-    render json: "your name is #{input_value.upcase}"
+    output_message = input_value.upcase
+    render json: {message: output_message}
   end
-  def name_letter_first
-    input_value = params["your_name"]
-    if input_value.downcase.first == "a"
-      render json: "hey your name starts with the first letter of the alphabet"
+  def guess_query
+    winning_number = 32
+    input_guess = params["the_users_guess"].to_i
+    if input_guess > winning_number
+      output_message = "guess lower"
+    elsif input_guess < winning_number
+      output_message = "guess higher"
     else
-      render json: "hey your name doesnt start with the first letter of the alphabet"
+      output_message = "you win"
     end
-  end
+    render json: {message: output_message}
+  end  
+#  def name_letter_first
+ #   input_value = params["your_name"]
+  #  if input_value.downcase.first == "a"
+   #   render json: "hey your name starts with the first letter of the alphabet"
+    #else
+     # render json: "hey your name doesnt start with the first letter of the alphabet"
+    #end
+  #end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/name_enter" => "params#name_enter"
   get "name_letter_first" => "params#name_letter_first"
+  get "/guess_query" => "params#guess_query"
 end
